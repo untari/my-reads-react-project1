4d2d2e560f2584106fdb03577741f454d385c875
untari <sastraembun0306@gmail.com>
2021-05-07 12:03:26 +0700

    create query function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom';
 import BookShelf from './BookShelf';
-import BookShelf from './SearchPage';
+import SearchPage from './SearchPage';
 import './App.css'
 
 class BooksApp extends React.Component {
+    state = {
+        query: '',
+        results: [],
+        books: []
+    }
+    componentDidMount() {
+        BooksAPI.getAll()
+        .then(data => {
+            this.setState({ books: data });
+        });
+    }
+    searchBooks = query => {
+        this.setState({ query: query });
+        if(query.length >0){
+            BooksAPI.search(query)
+            .then(data => {
+                this.seState({
+                    results: data
+                });
+            });
+        } else{
+            while (this.state.results.length > 0){
+                this.state.results.pop();
+            }
+            this.setState(() => ({ query: ''}));
+        }
+    };
 
   render() {
     return (
