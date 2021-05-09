4fc64b69b505a0e0e9f8043a820bef5b77788d13
untari <sastraembun0306@gmail.com>
2021-05-09 15:44:35 +0700

    create books title

---
+++ src/Components/TitleBooks.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Title = (props) => {
+    return(
+        <div className='list-books-title'>
+            My Reads 
+        </div>
+    );
+}
+export default Title;
