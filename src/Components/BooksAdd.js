4fa15e82ea9798119814e4edc0f06921f04dc68a
untari <sastraembun0306@gmail.com>
2021-05-09 15:45:02 +0700

    create component to add books

---
+++ src/Components/BooksAdd.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+function BooksAdd = () => {
+    return(
+        <div className='open-search'>
+            <Link to='/search'>
+            Book to add
+        </div>
+    )
+}
+export default BooksAdd;
